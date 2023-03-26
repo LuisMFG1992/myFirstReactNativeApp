62a3c0ec7778954be82ac7fb0d5d2c518fc5688f
LuisMFG <luismiguelfloresgr@gmail.com>
2023-03-26 09:51:04 +0200

    screensContainer styles created

--- App.js
+++ App.js
@@ -1,32 +1,28 @@
-import {
-  // StatusBar,
-  // Platform,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { StatusBar, Platform, StyleSheet, Text, View } from "react-native";
 import AppButton from "./app/components/AppButton";
 import Card from "./app/components/Card";
 
-// import ViewImageScreen from "./app/Screens/ViewImageScreen";
 import WelcomeScreen from "./app/Screens/WelcomeScreen";
+import ViewImageScreen from "./app/Screens/ViewImageScreen";
 
 export default function App() {
   return (
-    <Card
-      title={"Hey"}
-      subtitle={"How are you?"}
-      image={require("./app/assets/pexels-malte-luk-2244746.jpg")}
-    />
+    <View style={styles.screensContainer}>
+      <ViewImageScreen />
+    </View>
+    // <View style={[styles.screensContainer, { backgroundColor: "#f8f4f4", flex: 1 }]}>
+    //   <Card
+    //     title={"Hey"}
+    //     subtitle={"How are you?"}
+    //     image={require("./app/assets/pexels-malte-luk-2244746.jpg")}
+    //   />
+    // </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  screensContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : "0",
   },
 });
-
-// paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : "0",
